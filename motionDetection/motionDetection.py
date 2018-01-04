44e80e8db550040d081c206ac6c3956364e56a45
Rafael Acurcio <rafael.acurcio@gmail.com>
2018-01-03 23:09:47 -0300

    Changed codec and video size in order to make it work... programmagic

--- motionDetection/motionDetection.py
+++ motionDetection/motionDetection.py
@@ -1,4 +1,4 @@
-import numpy as np
+# import numpy as np
 import cv2
 from time import localtime, strftime
 from collections import deque
@@ -30,72 +30,74 @@ def isMoving(frame, sensibility):
         Raises:
 
     """
-    ##calculates the amount of "motion"
-    motionQuantity = sum(sum(frame/255))
+    # #calculates the amount of "motion"
+    motionQuantity = sum(sum(frame / 255))
     if (motionQuantity > sensibility):
         return True
     return False
 
-logger.info("Motion Detection started" )
 
-##VideoCapture object
+logger.info("Motion Detection started")
+
+# #VideoCapture object
 cap = cv2.VideoCapture(0)
-##BackgroundSubtractor object
+# #BackgroundSubtractor object
 fgbg = cv2.createBackgroundSubtractorMOG2()
-##CODEC used for recording
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-##VideoWriter object
-out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
-##Font object
+# #CODEC used for recording
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+w = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+h = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)   
+# #VideoWriter object
+out = cv2.VideoWriter('output.mp4', fourcc, 15.0, (int(w), int(h)))
+# #Font object
 font = cv2.FONT_HERSHEY_PLAIN
-##Create deque
+# #Create deque
 preRecording = deque(maxlen=20)
-##Variables
+# #Variables
 record = False
 sensibility = 1000
 
 while(1):
-    ##reads the frame
+    # #reads the frame
     ret, frame = cap.read()
 
-    ##creates a blur to reduce noise
-    blur = cv2.GaussianBlur(frame, (15,15),0)
-    ##generates mask
+    # #creates a blur to reduce noise
+    blur = cv2.GaussianBlur(frame, (15, 15), 0)
+    # #generates mask
     fgmask = fgbg.apply(blur)
-    ##thresholds the mask
+    # #thresholds the mask
     thresh = cv2.threshold(fgmask, 25, 255, cv2.THRESH_BINARY)[1]
-    ##Add frame to deque
+    # #Add frame to deque
     preRecording.append(frame)
 
-    if (isMoving(thresh,sensibility)):
+    if (isMoving(thresh, sensibility)):
         record = True
         counter = 50
 
     if(record or counter > 0):
-        ##Writes the date and time in the frame
-        ##putText(frame,text,location,font,size,color,thickness,aliasing)
+        # #Writes the date and time in the frame
+        # #putText(frame,text,location,font,size,color,thickness,aliasing)
         aux = preRecording.popleft()
-        cv2.putText(aux, strftime("%Y-%m-%d %H:%M:%S", localtime()), (10,20), font, 1.4, (0,0,0), 1, cv2.LINE_AA)
-        cv2.putText(frame, "*", (300,20), font, 1.4, (0,0,0), 1, cv2.LINE_AA)
-        ##Writes frame into video
+        cv2.putText(aux, strftime("%Y-%m-%d %H:%M:%S", localtime()), (10, 20), font, 1.4, (0, 0, 0), 1, cv2.LINE_AA)
+        cv2.putText(frame, "*", (300, 20), font, 1.4, (0, 0, 0), 1, cv2.LINE_AA)
+        # #Writes frame into video
         out.write(aux)
-        counter -=1
+        counter -= 1
 
     if(counter == 0):
         record = False
 
-    ##Show images
-    cv2.imshow('frame',frame)
-    #cv2.imshow('fgmask',fgmask)
-    #cv2.imshow('thresh',thresh)
+    # #Show images
+    cv2.imshow('frame', frame)
+    # cv2.imshow('fgmask',fgmask)
+    # cv2.imshow('thresh',thresh)
 
-
-    ##Press ESC to exit
+    # #Press ESC to exit
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
 
-logger.info("Shutdown motion detection" )
+logger.info("Shutdown motion detection")
 cap.release()
-cv2.destroyAllWindows()
 out.release()
+cv2.destroyAllWindows()
