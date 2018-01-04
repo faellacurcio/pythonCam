d68008be857c79520f6b3a0397904fbaafb1d308
Rafael Acurcio <rafael.acurcio@gmail.com>
2018-01-03 23:28:20 -0300

    Changed codec again...now it doesn't display the codec warning

--- motionDetection/motionDetection.py
+++ motionDetection/motionDetection.py
@@ -5,16 +5,20 @@ from collections import deque
 # loggex.py
 import logging
 
-
+# ========= Logger Setup =========
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
-
 fh = logging.FileHandler('events.log')
-
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
 fh.setFormatter(formatter)
 logger.addHandler(fh)
+
+videoExt = ".avi"
+videoFolder = "./videos/"
+
+
+def getTime():
+    return strftime("%Y-%m-%d %H:%M:%S", localtime())
 
 
 def isMoving(frame, sensibility):
@@ -44,11 +48,11 @@ cap = cv2.VideoCapture(0)
 # #BackgroundSubtractor object
 fgbg = cv2.createBackgroundSubtractorMOG2()
 # #CODEC used for recording
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
 w = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
 h = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)   
 # #VideoWriter object
-out = cv2.VideoWriter('output.mp4', fourcc, 15.0, (int(w), int(h)))
+out = cv2.VideoWriter(videoFolder + getTime() + videoExt, fourcc, 15.0, (int(w), int(h)))
 # #Font object
 font = cv2.FONT_HERSHEY_PLAIN
 # #Create deque
@@ -78,11 +82,14 @@ while(1):
         # #Writes the date and time in the frame
         # #putText(frame,text,location,font,size,color,thickness,aliasing)
         aux = preRecording.popleft()
-        cv2.putText(aux, strftime("%Y-%m-%d %H:%M:%S", localtime()), (10, 20), font, 1.4, (0, 0, 0), 1, cv2.LINE_AA)
+        cv2.putText(aux, getTime(), (10, 20), font, 1.4, (0, 0, 0), 1, cv2.LINE_AA)
         cv2.putText(frame, "*", (300, 20), font, 1.4, (0, 0, 0), 1, cv2.LINE_AA)
         # #Writes frame into video
         out.write(aux)
         counter -= 1
+
+    if(counter == 1):
+        out = cv2.VideoWriter(videoFolder + getTime() + videoExt, fourcc, 15.0, (int(w), int(h)))
 
     if(counter == 0):
         record = False
